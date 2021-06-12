27b69fadab11530e88c6b6672c16a0e67a7cddce
Wagner Tironi Pinto <wagner.pinto@adsprev.com.br>
2021-06-12 11:36:06 -0300

    Construindo um projeto de uma API.NET integrada ao MongoDB projato DIO

---
+++ Api/Data/MongoDB.cs
@@ -0,0 +1,44 @@
+using System;
+using Api.Data.Collections;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace Api.Data
+{
+    public class MongoDB
+    {
+        public IMongoDatabase DB { get; }
+
+        public MongoDB(IConfiguration configuration)
+        {
+            try
+            {
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(configuration["ConnectionString"]));
+                var client = new MongoClient(settings);
+                DB = client.GetDatabase(configuration["NomeBanco"]);
+                MapClasses();
+            }
+            catch (Exception ex)
+            {
+                throw new MongoException("Deu ruim ao conectar no MongoDB", ex);
+            }
+        }
+
+        private void MapClasses()
+        {
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Infectado)))
+            {
+                BsonClassMap.RegisterClassMap<Infectado>(i =>
+                {
+                    i.AutoMap();
+                    i.SetIgnoreExtraElements(true);
+                });
+            }
+        }
+    }
+}
